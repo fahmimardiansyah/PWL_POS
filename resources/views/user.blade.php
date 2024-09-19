08759f19923c693e868aa70fa7985155aeb765ec
fahmimardiansyah <boilaikskin@gmail.com>
2024-09-19 21:56:42 +0700

    Js4(Prak 2.3)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -9,18 +9,16 @@
 <body>
     <h1>Data User</h1>
     <table border="1" cellpading="2" cellspacing="0">
-        <tr>
-            <td>ID</td>
-            <td>Username</td>
-            <td>Nama</td>
-            <td>ID Level Pengguna</td>
-        </tr>
-        <tr>
-            <td>{{ $data->user_id }}</td>
-            <td>{{ $data->username }}</td>
-            <td>{{ $data->nama }}</td>
-            <td>{{ $data->level_id }}</td>
-        </tr>   
+        <thead>
+            <tr>
+                <th>Jumlah Pengguna</th>
+            </tr> 
+        </thead>
+        <tbody>
+            <tr>
+                <td>{{ $data }}</td>
+            </tr>
+        </tbody>
     </table>
 </body>
 </html>
