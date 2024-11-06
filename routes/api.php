afec16b99362073256c0608ff0c8127b92c59242
fahmimardiansyah <ianfahmi12@gmail.com>
2024-11-06 10:27:42 +0700

    JS10(Prak 4-CRUD Level)

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
 use App\Http\Controllers\Api\LogoutController;
+use App\Http\Controllers\Api\LevelController;
 use App\Http\Controllers\Controller;
 
 /*
@@ -24,3 +25,10 @@ Route::post("/logout", App\Http\Controllers\Api\LogoutController::class)->name('
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+    Route::get('/levels', [LevelController::class, 'index']);
+    Route::post('/levels', [LevelController::class, 'store']);
+    Route::get('/levels/{level}', [LevelController::class, 'show']);
+    Route::put('/levels/{level}', [LevelController::class, 'update']);
+    Route::delete('/levels/{level}', [LevelController::class, 'destroy']);
+
